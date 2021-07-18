17217986c04a14b63ae6944845e1d2c49dce5936
youngmu <tyg04308@naver.com>
2021-07-18 19:25:34 +0900

    make SRCNN model

---
+++ model.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+
+class SRCNN(nn.Module):
+    def __init__(self):
+        super(SRCNN, self).__init__()
+
+        self.patch_extraction_representation = nn.Sequential(
+            nn.Conv2d(in_channels=1,
+                out_channels=64,
+                kernel_size=9),
+            nn.ReLU(True))
+
+        self.non_linear_mapping = nn.Sequential(
+            nn.Conv2d(in_channels=64,
+                out_channels=32,
+                kernel_size=1),
+            nn.ReLU(True))
+
+        self.reconstruction = nn.Sequential(
+            nn.Conv2d(in_channels=32,
+                out_channels=1,
+                kernel_size=5))
+
+    def forward(self, x):
+        x = self.patch_extraction_representation(x)
+        x = self.non_linear_mapping(x)
+        x = self.reconstruction(x)
+
+        return x
+
+
+if __name__ == '__main__':
+    dummy_data = torch.rand(10, 1, 32, 32)
+
+    srcnn = SRCNN()
+
+    print ("SRCNN network")
+    print (srcnn)
+
+    print ("\n--------------------------------------\n")
+
+    x = srcnn(dummy_data)
+
+    print (f"Result: {x.shape}")
