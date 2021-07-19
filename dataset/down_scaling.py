030433039a059b789b1d972fb62fbd64d657320f
youngmu <tyg04308@naver.com>
2021-07-19 17:37:12 +0900

    down scaling val images

---
+++ dataset/down_scaling.py
@@ -0,0 +1,18 @@
+import os
+import cv2
+
+src_path = './val'
+dst_path = './train'
+
+file_list = os.listdir(src_path)
+
+for idx, image in enumerate(file_list):
+    val_img = cv2.imread(os.path.join(src_path, image), cv2.IMREAD_COLOR)
+
+    small_img = cv2.resize(val_img, (80, 80), interpolation=cv2.INTER_CUBIC)
+
+    big_img = cv2.resize(small_img, (240, 240), interpolation=cv2.INTER_CUBIC)
+
+    cv2.imwrite(os.path.join(dst_path, image), big_img) 
+
+    print (f"{idx+ 1} / {len(file_list)}")
