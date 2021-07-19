faabb6b18f2d205cce64c1e7a7f3be12eea0f864
youngmu <tyg04308@naver.com>
2021-07-19 17:14:32 +0900

    extract sub_image

--- dataset/sub_image.py
+++ dataset/sub_image.py
@@ -2,9 +2,9 @@ import os
 from PIL import Image
 import numpy as np
 
-SUBIMAGE_SIZE = 100
-STRIDE = 20
-PADDING = 20
+SUBIMAGE_SIZE = 240
+STRIDE = 200
+PADDING = 100
 
 # Numpy array shape (h, w, c)
 NUMPY_HEIGHT = 0
@@ -12,23 +12,32 @@ NUMPY_WIDHT = 1
 NUMPY_CHANNEL = 2
 
 origin_path = './origin'
-dst_path = './train'
+dst_path = './val'
 
 file_list = os.listdir(origin_path)
 
-for image in file_list:
+for idx, image in enumerate(file_list):
     origin_image = Image.open(os.path.join(origin_path, image))
     origin_arr = np.array(origin_image)
 
     start_x = PADDING
     start_y = PADDING
 
-    while start_y + SUBIMAGE_SIZE < origin_arr[NUMPY_HEIGHT] + PADDING:
+    cnt = 0
 
-        sub_image = origin_arr[start_y:start_y+SUBIMAGE_SIZE, start_x:start_x+SUBIMAGE_SIZE, :]
+    while start_y + SUBIMAGE_SIZE < origin_arr.shape[NUMPY_HEIGHT] + PADDING:
 
-        if start_x + STRIDE + SUBIMAGE_SIZE > origin_arr[NUMPY_WIDHT] + PADDING:
+        sub_image_arr = origin_arr[start_y:start_y+SUBIMAGE_SIZE, start_x:start_x+SUBIMAGE_SIZE, :]
+
+        sub_image = Image.fromarray(sub_image_arr)
+        sub_image.save(os.path.join(dst_path, image + "_" + str(cnt) + ".jpg"))
+
+        if start_x + STRIDE + SUBIMAGE_SIZE > origin_arr.shape[NUMPY_WIDHT] + PADDING:
             start_x = PADDING
             start_y += STRIDE
         else:
             start_x += STRIDE
+
+        cnt += 1
+
+    print (f"{idx+ 1} / {len(file_list)}")
